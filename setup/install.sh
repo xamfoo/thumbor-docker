dd4b20489750cf17b5ebb67cf591576db2d4ed06
xamfoo <xamfoo@all.repo>
2015-03-10 13:17:24 +0800

    Updated install steps

--- setup/install.sh
+++ setup/install.sh
@@ -1,38 +1,27 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
 
-echo "hello"
+cd "/setup"
 
 _install() {
-  local pkg="thumbor" ver=$1
-
-  if [ -e "requirements" ]; then
-    apt-get update && apt-get install \
-      --no-install-recommends --ignore-missing --fix-broken -y \
-        $(cat requirements) && \
-  fi
-
-  # Temporary install unzip
-  apt-get install unzip
-
-  # Download the snapshot
-  curl https://github.com/thumbor/$pkg/archive/${ver}.zip && \
-    unzip ${ver}.zip >/dev/null && rm -f ${ver}.zip
-
-  cd "$pkg-${ver}"
-
-  python setup.py install
-
-  # Remove unzip
-  apt-get purge unzip
-
+  apt-get update
+  apt-get install \
+    --no-install-recommends --ignore-missing --fix-broken -y \
+      $(cat requirements | grep -v "#")
   # Clean up APT when done
   apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-  cd ".."
-  rm -rf "/$pkg-${ver}"
 }
 
-_install thumbor $THUMBOR_VERSION
+if [ -e "requirements" ]; then
+  _install
+  cd "thumbor"
+else
+  cd "thumbor"
+  _install
+fi
+
+python setup.py install
+
+cd ".."
+rm -rf "thumbor"
